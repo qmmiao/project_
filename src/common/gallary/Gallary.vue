76b520b74c45738b9f988a23b2ea7eb752fdc366
qmmiao <985149328@qq.com>
2018-07-02 11:16:01 +0800

    gallary

--- src/common/gallary/Gallary.vue
+++ src/common/gallary/Gallary.vue
@@ -1,12 +1,9 @@
 <template>
-    <div class="container">
+    <div class="container" @click="handleGallaryClick">
         <div class="wrapper">
 						<swiper :options="swiperOption">
-							<swiper-slide >
-									<img class="gallary-img" src="http://img1.qunarzz.com/sight/p0/1803/12/126471e0966aca5ba3.img.jpg_r_800x800_fd14650b.jpg" alt="">
-							</swiper-slide>
-								<swiper-slide >
-									<img class="gallary-img" src="http://img1.qunarzz.com/sight/p0/1806/5b/5b32a9e1ac462fb5a3.img.jpg_r_800x800_ba8d7dcd.jpg" alt="">
+							<swiper-slide v-for="(item, index) in imgs" :key="index">
+								<img class="gallary-img" :src="item" alt=""/>
 							</swiper-slide>
 							<div class="swiper-pagination"  slot="pagination"></div>
 					</swiper>
@@ -21,12 +18,28 @@
 <script>
 export default {
 		name: "CommonGallary",
+		props: {
+			imgs: {
+				type: Array,
+				dafault () {
+					return [
+						]
+				}
+			}
+		},
 		data () {
 			return {
 				swiperOption: {
 					pagination: ".swiper-pagination",
-					paginationType: "fraction"
+					paginationType: "fraction",
+					observer:true,
+					observeParents:true,
 				}
+			}
+		},
+		methods: {
+			handleGallaryClick () {
+				this.$emit("close")
 			}
 		}
 }
@@ -35,28 +48,27 @@ export default {
 
 <style lang="stylus" scoped>
 .container >>> .swiper-container
-	overflow: inherit 
-.container
-	display: flex
-	flex-direction: column
-	justify-content: center
-	z-index: 99
-	position: fixed
-	top: 0
-	left: 0
-	right: 0
-	bottom: 0
-	background: #000
-	.wrapper
-		height: 0
-		width: 100%
-		// overflow: hidden
-		padding-bottom: 66%
-		.gallary-img
-			width: 100%
-		.swiper-pagination
-			color: #fff
-			bottom: -1rem
+    overflow: inherit
+  .container
+    display: flex
+    flex-direction: column
+    justify-content: center
+    z-index: 99
+    position: fixed
+    left: 0
+    right: 0
+    top: 0
+    bottom: 0
+    background: #000
+    .wrapper
+      height: 0
+      width: 100%
+      padding-bottom: 100%
+      .gallary-img
+        width: 100%
+      .swiper-pagination
+        color: #fff
+        bottom: -1rem
 			
 
 
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-    <div class="banner">
+    <div class="banner" @click="handleBannerClick">
         <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1803/12/126471e0966aca5ba3.img.jpg_600x330_8b04e389.jpg" alt="">
 				<div class="banner-info">
 					<p class="banner-title">大连圣亚海洋世界（AAAA景区）</p>
@@ -10,7 +10,7 @@
 					</p>
 				</div>
 		</div>
-		<common-gallary></common-gallary>
+		<common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
 </div>
 </template>
 
@@ -18,6 +18,21 @@
 import CommonGallary from "common/gallary/Gallary"
 export default {
 	name: "DetailBanner",
+	data () {
+		return {
+			showGallary: false,
+			imgs: ['http://img1.qunarzz.com/sight/p0/1803/12/126471e0966aca5ba3.img.jpg_r_800x800_fd14650b.jpg',
+						'http://img1.qunarzz.com/sight/p0/1806/5b/5b32a9e1ac462fb5a3.img.jpg_r_800x800_ba8d7dcd.jpg']
+		}
+	},
+	methods: {
+		handleBannerClick () {
+			this.showGallary = true
+		},
+		handleGallaryClose () {
+			this.showGallary = false
+		}
+	},
 	components: {
 		CommonGallary
 	}
